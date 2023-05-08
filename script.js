68a500b8a0ba099c9db34db2bdacb88958e1cd3d
Luke Hufton <lukehufton@gmail.com>
2023-05-08 10:31:40 -0500

    added source in comments for active effect on cells

--- script.js
+++ script.js
@@ -5,6 +5,19 @@ let botSequence = []
 let userSequence = []
 let level = 1
 
+const startButton = document.getElementById('Start')
+startButton.addEventListener('click', () => {
+  cells.forEach((cell) => {
+    cell.innerHTML = ''
+  })},
+
+function startGame() {
+  addEventListener('click', startGame)
+  botSequence = []
+  userSequence = []
+  level = 1
+  nextRound()
+},
 // https://www.w3schools.com/js/js_random.asp
 function getRandomColor() {
   const index = Math.floor(Math.random() * colors.length)
